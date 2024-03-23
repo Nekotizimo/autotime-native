b6de7bca2693ba2c2900da3f6ebd2c9df1d8adb6
Nekotizimo <kennycho168@outlook.com>
2024-03-23 22:59:00 +0800

    Added header bar with + button

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import { StrictMode } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, Pressable } from 'react-native';
 import { TimerList } from './components/TimerList';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { FontAwesome6 } from '@expo/vector-icons';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  
-
   return (
     <StrictMode>
       <SafeAreaView style={styles.container}>
-        <TimerList></TimerList>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="TimerList" component={TimerList} 
+              options={{ 
+                title: 'AutoTime',
+                headerRight: () => (
+                  <Pressable onPress={() => alert('This is a button!')}>
+                    <FontAwesome6 name="plus" size={20} color="black" />
+                  </Pressable>
+                ),
+              }} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeAreaView>
     </StrictMode>
   );
